5ad21a63320b14d4d86e1fe843ba081d6cb300b0
Shree Ram Neupane <shreeramneupane0@gmail.com>
2016-07-04 13:19:40 +0545

    DATABASE setup with Mongoose ODM

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 'use strict';
+require('./models/index');
 
-var SwaggerExpress = require('swagger-express-mw');
-var app = require('express')();
+var SwaggerExpress = require('swagger-express-mw'),
+app = require('express')();
 module.exports = app; // for testing
 
 var config = {
   appRoot: __dirname // required config
 };
 
-SwaggerExpress.create(config, function(err, swaggerExpress) {
-  if (err) { throw err; }
+SwaggerExpress.create(config, function (err, swaggerExpress) {
+  if (err) {
+    throw err;
+  }
 
   // install middleware
   swaggerExpress.register(app);
---
+++ models/index.js
@@ -0,0 +1,49 @@
+var env = process.env.NODE_ENV || 'development';
+var fs = require('fs');
+var path = require('path');
+
+var dbConfig = require(__dirname + '/../config/env/db.json');
+
+// Bring Mongoose into the app
+var mongoose = require('mongoose');
+
+// Build the connection string
+var dbURI = dbConfig[env];
+
+// Create the database connection
+mongoose.connect(dbURI);
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error', function (err) {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', function () {
+  mongoose.connection.close(function () {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
+
+
+// BRING IN YOUR ALL SCHEMAS & MODELS
+fs
+.readdirSync(__dirname)
+.filter(function (file) {
+  return (file.indexOf('.') !== 0) && (file !== path.basename(module.filename)) && (file.slice(-3) === '.js');
+})
+.forEach(function (file) {
+  require('./' + file);
+});
