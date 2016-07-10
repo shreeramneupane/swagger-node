76fd04c1ec1079db01a9baaf1b603769fc6e7fed
Shree Ram Neupane <shreeramneupane0@gmail.com>
2016-07-10 10:52:36 +0545

    CONFIGURE make test configuration file public

--- models/index.js
+++ models/index.js
@@ -3,14 +3,14 @@
 var env = process.env.NODE_ENV || 'development';
 var fs = require('fs');
 var path = require('path');
-console.log(process.env.NODE_ENV, '(((((((((((((((((((((((((((((((((((((((((((')
-var dbConfig = require(__dirname + '/../config/env/' + env + '.json')["dbConfig"];
+
+var dbConfig = require(__dirname + '/../config/env/' + env + '.json').dbConfig;
 
 // Bring Mongoose into the app
 var mongoose = require('mongoose');
 
 // Build the connection string
-var dbURI = dbConfig["DB_URI"];
+var dbURI = dbConfig.DB_URI;
 
 // Create the database connection
 mongoose.connect(dbURI);
